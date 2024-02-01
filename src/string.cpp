543d3f75b4c85bd1931d5158da601a1bd0ffe498
Zhen Ye <zye14@circinus-11.ics.uci.edu>
2024-01-31 20:37:23 -0800

    submission commit for hw2

--- src/string.cpp
+++ src/string.cpp
@@ -20,7 +20,7 @@ String::String(const String &s) {
 
 char *String::strncpy(char *dest, const char *src, int n) {
     int i;
-    for (i = 0; i < n - 1 && src[i] != '\0'; i++) {
+    for (i = 0; i < n && src[i] != '\0'; i++) {
         dest[i] = src[i];
     }
     dest[i] = '\0'; // Ensure null-termination
