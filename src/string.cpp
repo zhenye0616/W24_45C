2a442a45fded8e4833d2e786a59a5c25315a2c28
Zhen Ye <zye14@circinus-48.ics.uci.edu>
2024-02-05 14:49:52 -0800

    submission commit for hw3

--- src/string.cpp
+++ src/string.cpp
@@ -31,7 +31,7 @@ char *String::strncpy(char *dest, const char *src, int n) {
 
 char *String::strcpy( char * dest, const char * src )
 {
-      int i = 0;
+      int i;
       for ( i = 0; src[i] != '\0'; ++i)  
       {
 	    dest[i] = src[i];
