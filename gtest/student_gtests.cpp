612b72fb5cec01ea75eedf131570b2f9c622ea14
Zhen Ye <zye14@circinus-48.ics.uci.edu>
2024-02-07 12:46:40 -0800

    Submission Commit for HW4.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -59,18 +59,43 @@ TEST(StringFunction, strncat) {
 }
 
 
+
 TEST(StringFunction, strcmp) {
-    const char* str1 = "abc";
-    const char* str2 = "abd";
-    int result = strcmp(str1, str2);
-    EXPECT_LT(result, 0); // Expect str1 < str2
+    /*char s1[] = "apple";
+    char s2[] = "banana";
+    char s3[] = "hello";
+    char s4[] = "hello";
+    EXPECT_EQ(strcmp(s3, s4), String::strcmp(s3, s4));
+    EXPECT_EQ(String::strcmp(s3, s4), 0);
+    EXPECT_EQ(strcmp(s1, s2), String::strcmp(s1, s2));
+    EXPECT_LT(String::strcmp(s1, s2), 0);
+    EXPECT_GT(String::strcmp(s2, s1), 0);*/
+    //compare nullptr leads to undefined behavior
+    char str1[] = "answer1";
+    char str2[] = "answer2";
+    char str3[] = "answer3";
+    EXPECT_EQ(String::strcmp(str2, str2), 0);
+    EXPECT_LT(String::strcmp(str2, str3), 0);
+    EXPECT_GT(String::strcmp(str2, str1), 0);
 }
 
 TEST(StringFunction, strncmp) {
-    const char* str1 = "abc";
-    const char* str2 = "abd";
-    int result = strncmp(str1, str2, 2); // Only compare first 2 characters
-    EXPECT_EQ(result, 0); // Expect str1 and str2 to be equal for the first 2 characters
+    const char* s1 = "";
+    const char* s2 = "";
+    const char* s3 = "hello";
+    const char* s4 = "hello";
+    const char* s5 = "world";
+    const char* s6 = "hello world";
+    const char* s7 = "jello";
+    const char* s8 = "hejlo";
+    EXPECT_EQ(String::strncmp(s1, s2, 5), 0);
+    EXPECT_EQ(String::strncmp(s3, s4, 5), 0);
+    EXPECT_NE(String::strncmp(s3, s5, 5), 0);
+    EXPECT_EQ(String::strncmp(s4, s6, 5), 0);
+    EXPECT_LT(String::strncmp(s4, s6, 11), 0);
+    EXPECT_EQ(String::strncmp(s4, s6, 3), 0);
+    EXPECT_LT(String::strncmp(s3, s7, 5), 0);
+    EXPECT_GT(String::strncmp(s3, s8, 5), 0);
 }
 
 TEST(StringFunction, strchr) {
@@ -81,10 +106,40 @@ TEST(StringFunction, strchr) {
 }
 
 TEST(StringFunction, strstr) {
-    const char* haystack = "Hello, World!";
-    const char* needle = "World";
-    const char* result = String::strstr(haystack, needle);
-    EXPECT_STREQ(result, "World!"); // Compare as C-strings
+    // Test strings and substrings
+    const char *str1 = "hello world";
+    const char *sub1 = "world";
+    const char *sub2 = "foo";
+    const char *sub3 = "";
+    const char *sub4 = "hello world";
+    const char *str2 = "hello hello world";
+    const char *sub6 = "hello";
+    const char *str3 = "hello hellhello world";
+    const char *sub7 = "hellhello";
+
+    // Test cases
+    const char* res1 = String::strstr(str1, sub1);
+    const char* res2 = String::strstr(str1, sub2);
+    const char* res3 = String::strstr(str1, sub3);
+    const char* res4 = String::strstr(sub3, sub2);
+    const char* res5 = String::strstr(str1, sub4);
+    const char* res6 = String::strstr(str2, sub6);
+    const char* res7 = String::strstr(str3, sub7);
+
+    // Assertions to compare with the standard strstr results
+    EXPECT_EQ(res1, strstr(str1, sub1));
+    EXPECT_EQ(res2, strstr(str1, sub2));
+    EXPECT_EQ(res3, strstr(str1, sub3));
+    EXPECT_EQ(res4, strstr(sub3, sub2));
+    EXPECT_EQ(res5, strstr(str1, sub4));
+    EXPECT_EQ(res6, strstr(str2, sub6));
+    EXPECT_EQ(res7, strstr(str3, sub7));
+
+    // Additional checks for the complex case
+    const char *res = String::strstr(str3, sub7);
+    EXPECT_NE(res, nullptr);  // Ensure a match was found
+    cout << res << endl;  // For debugging: print the match
+    EXPECT_EQ(strcmp(res, "hellhello world"), 0);  // Verify the match is correct
 }
 
 TEST(StringFunction, reverse_cpy) {
